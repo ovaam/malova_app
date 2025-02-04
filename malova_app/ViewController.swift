1d5f50b824afb5ff28844960c743d09331b2257d
ovaam <oamalova@edu.hse.ru>
2025-02-04 06:22:26 +0300

    Initial Commit

---
+++ malova_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  malova_app
+//
+//  Created by Малова Олеся on 04.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
